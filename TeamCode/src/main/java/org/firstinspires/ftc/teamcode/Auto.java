82decb5f9b983a2d19512eb87af22c9c1d20c736
FrogFrogFrogFrogFrogFrogFrogFrogFr <kadenkah13@gmail.com>
2026-07-20 11:31:12 -0400

    Update Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Null Auto")
 public class Auto extends LinearOpMode {
 
+    public double ticksPerInch = 0;
+    public double ticksPerDegree = 0;
     public Hardware robot;
 
     //When you press init
@@ -73,6 +76,70 @@ public class Auto extends LinearOpMode {
         robot.arm.setPower(.3);
         sleep(time);
         robot.arm.setPower(.3);
+    }
+
+    public void forwardEncoder(double power, double inches) {
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (inches * ticksPerInch);
+
+        robot.left.setTargetPosition(ticks);
+        robot.right.setTargetPosition(ticks);
+
+        robot.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.left.setPower(power);
+        robot.left.setPower(power);
+    }
+
+    public void turnLeftEncoder(double power, double inches) {
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (inches * ticksPerDegree);
+
+        robot.left.setTargetPosition(ticks);
+        robot.right.setTargetPosition(-ticks);
+
+        robot.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.left.setPower(power);
+        robot.left.setPower(power);
+    }
+
+    public void turnRightEncoder(double power, double inches) {
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (inches * ticksPerDegree);
+
+        robot.left.setTargetPosition(-ticks);
+        robot.right.setTargetPosition(ticks);
+
+        robot.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.left.setPower(power);
+        robot.left.setPower(power);
+    }
+
+    public void backwardEncoder(double power, double inches) {
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int ticks = (int) (inches * ticksPerInch);
+
+        robot.left.setTargetPosition(ticks);
+        robot.right.setTargetPosition(ticks);
+
+        robot.left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.left.setPower(-power);
+        robot.left.setPower(-power);
     }
 
 }
